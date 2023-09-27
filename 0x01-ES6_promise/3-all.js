ee362bab637d3030b06de7d931d9b10b14720187
mideactive <jenyoolumide@yahoo.com>
2023-09-27 15:13:42 +0000

    handle profile signup

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,20 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
+export default function handleProfileSignup() {
+  let body;
+  let firstName;
+  let lastName;
 
-  Promise.all(promises)
-    .then((results) => {
-      const [photoResponse, userResponse] = results;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  return uploadPhoto()
+    .then((data) => {
+      body = data.body;
+      createUser()
+        .then((data) => {
+          firstName = data.firstName;
+          lastName = data.lastName;
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
-    .catch((error) => {
-      console.error('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
