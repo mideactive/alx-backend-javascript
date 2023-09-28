ad0f4630c0a21342e11a44e81f6506046e8a0f77
mideactive <jenyoolumide@yahoo.com>
2023-09-28 14:45:45 +0000

    initialize rooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+// 1-make_classrooms.js
+import ClassRoom from './0-classroom.js';
+export default function initializeRooms() {
+	const sizes = [19, 20, 34];
+	const classrooms = sizes.map((size) => new ClassRoom(size));
+	return classrooms;
+}
