caa7722afa757cc0ed8e8ec86e6b5af37e7a3e5f
mideactive <jenyoolumide@yahoo.com>
2023-09-28 14:32:57 +0000

    max students size

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,6 @@
 // 0-classroom.js
-
 export default class ClassRoom {
-	constructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
-	}
+  constructor(maxStudentSize) {
+    this._maxStudentsSize = maxStudentSize;
+  }
 }
