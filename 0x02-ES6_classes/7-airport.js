459a69a3ea46175055b110a2eb5d74f0e166cc65
mideactive <jenyoolumide@yahoo.com>
2023-09-28 17:50:00 +0000

    return description airport code

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+// 7-airport.js
+export default class Airport {
+	constructor(name, code) {
+		this._name = name;
+		this._code = code;
+	}
+	get [Symbol.toStringTag]() {
+		return this._code;
+	}
+}
