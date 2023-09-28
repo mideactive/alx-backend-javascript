04434cf2133ba6345541556432521cc4ee5ee655
mideactive <jenyoolumide@yahoo.com>
2023-09-28 16:30:53 +0000

    currency name, code and convertion Rate

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,28 +1,40 @@
 // 4-pricing.js
 
-import Currency from './3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
-	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
-	get amount() {
-		return this._amount;
-	}
-	set amount(newAmount) {
-		this._amount = newAmount;
-	}
-	get currency() {
-		return this._currency;
-	}
-	set currency(newCurrency) {
-		this._currency = newCurrency;
-	}
-	displayFullPrice() {
-		return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-	}
-	static convertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  constructor(amount, currency) {
+    // Create objs
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Methods
+
+  displayFullPrice() {
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+
+  // Setters
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+    this._currency = newCurrency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
 }
