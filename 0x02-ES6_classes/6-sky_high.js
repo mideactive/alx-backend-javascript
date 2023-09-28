b878eb00f03b81e71a3f2917820229f63c0d6b7d
mideactive <jenyoolumide@yahoo.com>
2023-09-28 17:12:29 +0000

    inherit class Building

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,18 +2,27 @@
 import Building from './5-building'
 
 export default class SkyHighBuilding extends Building {
-	constructor(sqft, floors) {
-		super(sqft);
-		this._floors = floors;
-	}
-	evacuationWarningMessage() {
-                return `Evacuate slowly the ${this._floors} floors`;
-        }
+  constructor(sqft, floors) {
+    // call constructor of super class (Building)
+    super(sqft);
 
-	get sqft() {
-		return this._sqft;
-	}
-	get floors() {
-		return this._floors;
-	}
+    // Create objs
+    this._floors = floors;
+  }
+
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+
+  // Setters
+
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
 }
