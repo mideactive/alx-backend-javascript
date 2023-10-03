343e202b5bca82dd7d9a276e227f6ff6b5ae222a
mideactive <jenyoolumide@yahoo.com>
2023-10-03 10:36:54 +0000

    update grades using filter&map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,25 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-       return students.filter((student) => student.location === city)
-                      .map((student) => {
-			      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
-			      const grade = matchingGrade ? matchingGrade.grade : 'N/A';
-			      return { ...student, grade };
-		      });
+export default function updateStudentGradeByCity(
+  students,
+  city,
+  newGrades,
+) {
+  const filterStudents = students.filter(
+    (student) => student.location === city,
+  );
+
+  const gradeStudents = filterStudents.map(
+    (student) => {
+      for (const gradeInfo of newGrades) {
+        if (student.id === gradeInfo.studentId) {
+          student.grade = gradeInfo.grade; // eslint-disable-line no-param-reassign
+        }
+      }
+      if (student.grade === undefined) {
+        student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+      }
+      return student;
+    },
+  );
+
+  return gradeStudents;
 }
